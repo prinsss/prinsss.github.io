666e2468760e63116154a1d8edb6b3441057ae8e
printempw <printempw@gmail.com>
2017-10-04 16:10:10 +0800

    :construction_worker: Add Travis CI

---
+++ .travis/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ev
+
+# 先 clone 再 commit，避免直接 force commit
+git clone -b master git@github.com:printempw/printempw.github.io.git .deploy_git
+
+cd .deploy_git
+git checkout master
+mv .git/ ../public/
+cd ../public
+
+git add .
+git commit -m "Site updated: `date +"%Y-%m-%d %H:%M"`"
+
+# 同时 push 一份到自己的服务器上
+git remote add vps git@prinzeugen.net:hexo.git
+
+git push vps master:master --force --quiet
+git push origin master:master --force --quiet
