e3c9442abd33716a954649992d0f03721f7624f8
printempw <printempw@gmail.com>
2018-11-08 01:08:01 +0800

    :green_heart: Stick Node.js version to LTS on CI

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,8 +12,4 @@ cp -r ../public/* ./
 git add .
 git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
 
-# 同时 push 一份到自己的服务器上
-git remote add vps git@prinzeugen.net:hexo.git
-
-git push vps master:master --force --quiet
 git push origin master:master --force --quiet
